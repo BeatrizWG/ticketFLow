ca753a8c9b7cc19642d7c44f566a4790dbc97539
Beatriz Wanderley <143552212+BeatrizWG@users.noreply.github.com>
2025-05-07 16:03:28 -0300

    fix: correção na importação do CSS

--- front/src/components/sideBar/SideBar.jsx
+++ front/src/components/sideBar/SideBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; 
-import "./Sidebar.css";
+import "./SideBar.css";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
